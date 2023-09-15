1fee9dc8c7f8c1735240e6c630f863a073d9c355
Leonardo Rabero Lescano <leonardolescano14@gmail.com>
2023-09-15 16:05:19 -0400

    Adição de uma funcionalidade simples para baixar os checkpoints de exemplo direto.

--- url.py
+++ url.py
@@ -3,5 +3,5 @@ url = {
     'fovea' : "https://download.openmmlab.com/mmdetection/v2.0/foveabox/fovea_r50_fpn_4x4_1x_coco/fovea_r50_fpn_4x4_1x_coco_20200219-ee4d5303.pth",
     'faster' : "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth",
     'retinanet' : "https://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_fpn_1x_coco/retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth",
-    'atss' : "https://download.openmmlab.com/mmdetection/v2.0/atss/atss_r101_fpn_1x_coco/atss_r101_fpn_1x_20200825-dfcadd6f.pth"
+    'atss' : "https://download.openmmlab.com/mmdetection/v2.0/atss/atss_r50_fpn_1x_coco/atss_r50_fpn_1x_coco_20200209-985f7bd0.pth"
 }
