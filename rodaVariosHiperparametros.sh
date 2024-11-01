f1422804182e83752e44f76563975b8e6a52bdec
Hemerson Pistori <pistori@ucdb.br>
2024-11-01 07:44:52 -0400

    Permite rodar com diferentes hiperparametros

---
+++ rodaVariosHiperparametros.sh
@@ -0,0 +1,25 @@
+# Roda experimentos com as mesmas redes neurais, mas com diferentes hiperparâmetros
+#
+# Os resultados vão ficar em uma nova pasta chamada resultados
+
+lrS=(0.001)
+lcS=(0.1 0.01)
+liS=(0.1 0.01)
+
+rm -rf resultados
+mkdir -p resultados
+
+for lr in ${lrS[@]}; do
+    for lc in ${lcS[@]}; do
+        for li in ${liS[@]}; do
+            echo 'Rodando para lr=' $lr ' lc=' $lc ' li=' $li
+            python experimento.py -lr=$lr -lc=$lc -li=$li
+            mkdir -p resultados/lr$lr-lc$lc-li$li
+            cp -R dataset ./resultados/lr$lr-lc$lc-li$li
+            cp nohup.out ./resultados/lr$lr-lc$lc-li$li/
+            cp experimento.py ./resultados/lr$lr-lc$lc-li$li/
+        done
+    done
+done
+
+
