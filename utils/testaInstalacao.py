e3c81cd887425b8d0ba8953aea4081788b196d32
Hemerson Pistori <pistori@ucdb.br>
2024-12-16 10:01:54 -0400

    Melhorando as mensagens

--- utils/testaInstalacao.py
+++ utils/testaInstalacao.py
@@ -1,5 +1,6 @@
 # Testa se a GPU está ok usando uma chamada para o sistema operacional e rodando no nvidia-smi
 import os
+print('Testando se os drives de GPU estão rodando mo Linux:')
 os.system('nvidia-smi')
 
 # Testa pytorch
@@ -7,15 +8,17 @@ import torch
 
 # Testa se a GPU está disponível
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(device)
+print('Esta executando o pytorch em: ',device)
 
 # Testa se o pytorch está instalado corretamente
+print('Realizando uma operação matricial com o python:')
 x = torch.empty(5, 3)
 print(x)
 
 # Testa se o MMCV está instalado corretamente
+print('Testando o MMCV:')
 from mmcv.ops import get_compiling_cuda_version, get_compiler_version
-print(get_compiling_cuda_version())
-print(get_compiler_version())
+print('CUDA Version pelo MMCV: ',get_compiling_cuda_version())
+print('Compiler Version pelo MMCV: ',get_compiler_version())
 
 
