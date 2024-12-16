9ecedaf9ba4bb329271383a57077c6239981cfd0
Hemerson Pistori <pistori@ucdb.br>
2024-12-16 09:50:38 -0400

    Código para testar instalação

---
+++ utils/testaInstalacao.py
@@ -0,0 +1,17 @@
+# Testa pytorch
+import torch
+
+# Testa se a GPU está disponível
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+# Testa se o pytorch está instalado corretamente
+x = torch.empty(5, 3)
+print(x)
+
+# Testa se o MMCV está instalado corretamente
+from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+print(get_compiling_cuda_version())
+print(get_compiler_version())
+
+
